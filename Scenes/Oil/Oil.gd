d61687bc2dfe9665aeba05587bb9245d7fc6a327
sohamastadion <sohamvagal223@gmail.com>
2026-03-10 22:52:17 +0530

    Oil slicks

---
+++ Scenes/Oil/Oil.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await get_tree().create_timer(
+		randf_range(3.0, 5.0)
+	).timeout
+	queue_free()
+
+
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is Car : print("oil _on_area_entered") # Replace with function body.
